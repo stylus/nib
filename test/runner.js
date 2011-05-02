5545dac8672a788242c05f9af1ce366e9f50c9dd
Tj Holowaychuk <tj@vision-media.ca>
2011-05-02 09:06:07 -0700

    copied test runner from stylus for now

--- test/runner.js
+++ test/runner.js
@@ -1,63 +1,157 @@
 
 require.paths.unshift(__dirname + '/../support');
 
+
 /**
  * Module dependencies.
  */
 
-var fs = require('fs')
+var stylus = require('stylus')
+  , basename = require('path').basename
   , nib = require('../')
-  , stylus = require('stylus');
+  , fs = require('fs');
 
-console.error();
+/**
+ * Test count.
+ */
 
-// files to test
+var count = 0;
 
-var files = process.argv.slice(2);
+/**
+ * Failure count.
+ */
 
-// test files in sequence
+var failures = 0;
 
-(function next(){
-  var file;
-  if (file = files.shift()) {
-    readFile(file, next);
-  } else {
-    console.error();
-  }
-})();
+/**
+ * Test the given `test`.
+ *
+ * @param {String} test
+ * @param {Function} fn
+ */
 
-// read test files
+function test(test, fn) {
+  var base = __dirname + '/cases/' + test
+    , path =  base + '.styl'
+    , csspath = base + '.css';
 
-function readFile(path, fn){
-  process.stdout.write('  \033[90m- ' + path + ':\033[0m ');
-  fs.readFile(path, 'utf8', function(err, styl){
+  fs.readFile(path, 'utf8', function(err, str){
     if (err) throw err;
-    fs.readFile(path.replace('.styl', '.css'), 'utf8', function(err, css){
+
+    var style = stylus(str)
+      .use(nib())
+      .set('filename', path)
+      .include(__dirname + '/images')
+      .include(__dirname + '/cases/import.basic');
+
+    if (~test.indexOf('compress')) style.set('compress', true);
+
+    style.render(function(err, actual){
       if (err) throw err;
-      testFile(path, styl, css, fn);
+      fs.readFile(csspath, 'utf8', function(err, expected){
+        if (err) throw err;
+        expected += '\n';
+        if (actual == expected) {
+          fn();
+        } else {
+          fn(actual, expected);
+        }
+      });
     });
+  });
+  return test;
+};
+
+/**
+ * Auto-load and run tests.
+ */
+
+fs.readdir(__dirname + '/cases', function(err, files){
+  if (err) throw err;
+  var tests = []
+    , curr;
+
+  files.forEach(function(file){
+    if (/\.styl$/.test(file)) {
+      ++count;
+      tests.push(basename(file, '.styl'));
+    }
+  });
+
+  (function next() {
+    curr = tests.shift();
+    if (!curr) return done();
+    process.stderr.write('    \033[90m' + curr + '\033[0m');
+    test(curr, function(actual, expected){
+      if (actual) {
+        ++failures;
+        console.error('\r  \033[31m✖\033[0m \033[90m' + curr + '\033[0m\n');
+        diff(actual, expected);
+        console.error();
+      } else {
+        console.error('\r  \033[36m✔\033[0m \033[90m' + curr + '\033[0m');
+      }
+      next();
+    });
+  })();
+});
+
+/**
+ * Diff `actual` / `expected`.
+ *
+ * @param {String} actual
+ * @param {String} expected
+ */
+
+function diff(actual, expected) {
+  var actual = actual.split('\n')
+    , expected = expected.split('\n')
+    , len = largestLineIn(expected);
+
+  console.error('  expected');
+  expected.forEach(function(line, i){
+    var other = actual[i]
+      , pad = len - line.length
+      , pad = Array(++pad).join(' ')
+      , same = line == other;
+    if (same) {
+      console.error('  %d| %j%s | %j', i+1, line, pad, other);
+    } else {
+      console.error('  \033[31m%d| %j%s | %j\033[0m', i+1, line, pad, other);
+    }
   });
 }
 
-// perform comparison
+/**
+ * Return the length of the largest line in `lines`.
+ *
+ * @param {Array} lines
+ * @return {Number}
+ */
 
-function testFile(path, styl, expected, fn) {
-  stylus(styl)
-    .set('filename', path)
-    .use(nib())
-    .render(function(err, css){
-      if (err) throw err;
-      if (css.trim() == expected.trim()) {
-        console.error('\033[36m✓\033[0m');
-        fn();
-      } else {
-        console.error('\033[31m✘\033[0m');
-        console.error('\n  \033[90mactual:\033[0m\n');
-        console.error(css.replace(/^/gm, '  '));
-        console.error('  \033[90mexpected:\033[0m\n');
-        console.error(expected.replace(/^/gm, '  '));
-        console.error();
-        process.exit(1);
-      }
-    });
+function largestLineIn(lines) {
+  return lines.reduce(function(n, line){
+    return Math.max(n, line.length);
+  }, 0);
+}
+
+/**
+ * Done!!!
+ */
+
+function done() {
+  console.log();
+  console.log(
+      '  \033[90mcompleted\033[0m'
+    + ' \033[32m%d\033[0m'
+    + ' \033[90mtests\033[0m', count);
+
+  if (failures) {
+    console.error('  \033[90mfailed\033[0m'
+      + ' \033[31m%d\033[0m'
+      + ' \033[90mtests\033[0m', failures);
+    process.exit(failures);
+  }
+
+  console.log();
 }
