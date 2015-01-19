a24215d87318f0d553f2b88ed2859b82b4188c10
Jorge Bucaran <jbucaran@me.com>
2015-01-19 10:53:50 -0500

    improve comments for test runner

--- test/runner.js
+++ test/runner.js
@@ -1,20 +1,24 @@
-
 /**
  * Module dependencies.
  */
-
 var stylus = require('stylus'),
     nib = require('../'),
     fs = require('fs');
 
-// test cases
-
+/**
+ * Read test/cases directory and filter all `.styl` files, then remove
+ * this extension for each file in the collection and prepare to test.
+ */
 var cases = fs.readdirSync('test/cases').filter(function(file){
-  return ~file.indexOf('.styl');
+  return ~file.indexOf('.styl'); // bitwise flip to treat result as truthy.
 }).map(function(file){
   return file.replace('.styl', '');
 });
 
+/*
+ * For each `.styl` and `.css` pair in `test/cases`, compile stylus to css
+ * and compare actual result to expected css.
+ */
 describe('integration', function(){
   cases.forEach(function(test){
     var name = test.replace(/[-.]/g, ' ');
